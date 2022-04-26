b771365c5e18b75daffc29a2032ce3aa3f214653
Luuk Boulogne <50451985+luukboulogne@users.noreply.github.com>
2022-04-26 09:10:29 +0200

    Update test.bat

--- training/test.bat
+++ training/test.bat
@@ -4,9 +4,9 @@ rmdir /S "%~dp0..\inference\artifact\"
 mkdir "%~dp0..\inference\artifact\"
 
 docker run --rm --gpus all^
- --memory=16g --memory-swap=16g^
+ --memory=108g --memory-swap=108g^
  --cap-drop=ALL --security-opt="no-new-privileges"^
- --network none --shm-size=128m --pids-limit 256^
+ --network none --shm-size=20g --pids-limit 256^
  -v %1:/input/^
  -v %~dp0..\inference\artifact\:/output/^
  stoictrain
