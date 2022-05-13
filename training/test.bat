1f3820cf08c0a3f77bce8d1dc0d495ec48cbfdfd
Luuk Boulogne <50451985+luukboulogne@users.noreply.github.com>
2022-05-13 11:50:28 +0200

    Add prompt before deleting

--- training/test.bat
+++ training/test.bat
@@ -1,5 +1,14 @@
 call .\build.bat
 
+@echo off
+:choice
+set /P c=I will now recursively delete the contents of  %~dp0..\inference\artifact\  and   %2 . Are you sure you want to continue? (type yes)? 
+if /I "%c%" EQU "yes" goto deleteandrun
+echo aborting
+exit
+
+:deleteandrun
+
 rmdir /S "%~dp0..\inference\artifact\"
 mkdir "%~dp0..\inference\artifact\"
 rmdir /S %2
