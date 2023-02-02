35dac4c5638a353ff1cae3ffd1cf597021946513
luukboulogne <luuk.boulogne@radboudumc.nl>
2023-02-02 09:34:14 +0100

    Training in docker container with decoupled preprocessing and training

--- training/main.py
+++ training/main.py
@@ -1,8 +1,12 @@
 import shutil
-from train import do_learning
+from train import do_learning as do_learning
+from do_preprocess import main as do_preprocessing
 import paths
 
 if __name__ == "__main__":
+    # Preprocessing
+    do_preprocessing(paths.PREPROCESS_INPUT_DIR, paths.PREPROCESS_OUTPUT_DIR)
+
     # Substitute do_learning for your training function.
     # It is recommended to write artifacts (e.g. model weights) to ARTIFACT_DIR during training.
     artifacts = do_learning(paths.TRAINING_INPUT_DIR, paths.TRAINING_OUTPUT_DIR)
